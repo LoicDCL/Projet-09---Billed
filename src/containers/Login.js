3e97600a9d5a9e07dead16e19baa4deeb2bb18dd
LoicDCL <loic.paingt@gmail.com>
2026-01-06 12:32:18 +0100

    Correction bug Login.js
    Correction du Bug 02 concernant l'affichage du Dashboard Admin.
    Erreur de testid, remplacement de "employee" par "admin"

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -36,13 +36,13 @@ export default class Login {
       })
 
   }
-
+// Bug Login 02 : Correction du testid, remplacement de "employee" par "admin"
   handleSubmitAdmin = e => {
     e.preventDefault()
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
